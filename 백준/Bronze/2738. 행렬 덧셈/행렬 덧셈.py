c9f4a3087b89c57f2bab81e9ae2e5dd9dcfa622e
ClairPark <108247628+ClairPark@users.noreply.github.com>
2024-12-29 16:24:32 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,15 @@
+a, b = map(int, input().split( ))
+c = []
+d = []
+
+
+for i in range (a):
+    c.append(list(map(int, input().split( ))))
+    
+for i in range (a):
+    d.append(list(map(int, input().split( ))))
+    
+for i in range (a):
+    for j in range (b):
+        print (c[i][j] + d[i][j], end = " ")
+    print ()
