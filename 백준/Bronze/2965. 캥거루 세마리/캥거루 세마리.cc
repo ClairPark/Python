715ebb61c763506e8ba1166c2feaa8cfa2a39eda
ClairPark <108247628+ClairPark@users.noreply.github.com>
2025-01-05 15:32:01 +0900

    [Bronze III] Title: 캥거루 세마리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2965. 캥거루 세마리/캥거루 세마리.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (c-b >= b-a)
+    {
+        cout << c - b - 1;
+    }
+    else
+    {
+        cout << b - a - 1;
+    }
+}
